a67e0ca6e60f24c02aef5b1f9ffc9ccadfb0e3f6
AnnaNguyen205 <156358256+AnnaNguyen205@users.noreply.github.com>
2025-02-17 10:14:46 -0500

    Update script
    big update for SCSS, JS modules, header, navigation, footer, index.html, and about.html.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { menu } from "./modules/menu.js";
+import { stories } from "./modules/stories.js";
+import { video } from "./modules/video.js";
+import { aboutAnimation } from "./modules/aboutAnimation.js";
+
+menu();
+stories();
+video();
+aboutAnimation();
---
+++ js/modules/aboutAnimation.js
@@ -0,0 +1,71 @@
+export function aboutAnimation() {
+  gsap.from(".about-header h1", {
+    opacity: 0,
+    y: -50,
+    duration: 1.5,
+    delay: 0.5,
+  });
+  gsap.from(".about-header p", { opacity: 0, y: 50, duration: 1.5, delay: 1 });
+
+  gsap.from(".project-background", {
+    scrollTrigger: {
+      trigger: ".project-background",
+      start: "top 80%",
+    },
+    opacity: 0,
+    y: 50,
+    duration: 1,
+  });
+
+  gsap.from(".mission-vision .mission", {
+    scrollTrigger: {
+      trigger: ".mission-vision",
+      start: "top 80%",
+    },
+    opacity: 0,
+    x: -50,
+    duration: 1,
+  });
+
+  gsap.from(".mission-vision .vision", {
+    scrollTrigger: {
+      trigger: ".mission-vision",
+      start: "top 80%",
+    },
+    opacity: 0,
+    x: 50,
+    duration: 1,
+  });
+
+  gsap.from(".phases .phase", {
+    scrollTrigger: {
+      trigger: ".phases",
+      start: "top 80%",
+    },
+    opacity: 0,
+    y: 50,
+    stagger: 0.3,
+    duration: 1,
+  });
+
+  gsap.from(".purpose ul li", {
+    scrollTrigger: {
+      trigger: ".purpose",
+      start: "top 80%",
+    },
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    duration: 1,
+  });
+
+  gsap.from(".location", {
+    scrollTrigger: {
+      trigger: ".location",
+      start: "top 80%",
+    },
+    opacity: 0,
+    y: 50,
+    duration: 1,
+  });
+}
