71d836c7449d9db099820889a6ff8ef8b05eb1b8
Emmanuel9494 <144351016+Emmanuel9494@users.noreply.github.com>
2025-04-08 13:17:21 -0400

    Donors Dynamic Section
    This section is built with Vue, and its dynamic, actual data is pulled from the database.

--- js/donors.js
+++ js/donors.js
@@ -2,8 +2,10 @@ import { menu } from "./modules/menu.js";
 import { headerText } from "./modules/headerText.js";
 import { donors } from "./modules/donors.js";
 import { FAQ } from "./modules/FAQ.js";
+import { donorsApp } from "./vue/vueDonors_List.js";
 
 menu();
 donors();
 headerText();
 FAQ();
+donorsApp();
