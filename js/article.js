99928df8bbba635539c511c9cc4df0492a230b8f
AnnaNguyen205 <156358256+AnnaNguyen205@users.noreply.github.com>
2025-04-09 14:33:12 -0400

    Clean Up
    cleaned up the redundant code

--- js/article.js
+++ js/article.js
@@ -1,7 +1,3 @@
 import { menu } from "./modules/menu.js";
-// import { headerText } from "./modules/headerText.js";
-// import { contact } from "./modules/contact.js";
 
 menu();
-// contact();
-// headerText();
--- js/vue/vueDonors_List.js
+++ js/vue/vueDonors_List.js
@@ -1,33 +1,33 @@
 export function donorsApp() {
-    const app = Vue.createApp({
-        data() {
-            return {
-                donors: [], 
-                error: ''
-            };
-        },
-        methods: {
-            fetchDonors() {
-                fetch('http://localhost/github/BrothersInArms-Level4-FIP/bia-backend/brothers_in_arms_fip_backend/public/donors')
-                .then(response => response.json())
-                    .then(data => {
-                        this.donors = data; // This will fetch all the donors data from the API
+  const app = Vue.createApp({
+    data() {
+      return {
+        donors: [],
+        error: "",
+      };
+    },
+    methods: {
+      fetchDonors() {
+        fetch(
+          "http://localhost:8888/BrothersInArms-Level4-FIP/bia-backend/brothers_in_arms_fip_backend/public/donors"
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            this.donors = data;
 
+            this.loading = false;
+          })
+          .catch((error) => {
+            this.error =
+              "Unable to load donors data. Please check Your connection.";
+            console.error(error);
+          });
+      },
+    },
+    mounted() {
+      this.fetchDonors();
+    },
+  });
 
-                        // But in a case where donors are becoming huge and we have multiple entries, we can setup a page by page data or instead limit the number of data in this script or the solution is endless, but for now lets say we Limit to 5 donors
-                        // this.donors = data.slice(0, 5); // This will limit the number of donors to 5
-                        this.loading = false;
-                    })
-                    .catch(error => {
-                        this.error = 'Unable to load donors data. Please check Your connection.';
-                        console.error(error);
-                    });
-            }
-        },
-        mounted() {
-            this.fetchDonors(); 
-        }
-    });
-
-    app.mount("#donorsApp");
+  app.mount("#donorsApp");
 }
